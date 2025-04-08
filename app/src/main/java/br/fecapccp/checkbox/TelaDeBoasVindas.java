87de79a716f1ed41f585484f8b0493c93307afc1
ViniSantos09 <ribeiro.vinicius@edu.fecap.br>
2025-04-08 20:14:27 -0300

    save

--- app/src/main/java/br/fecapccp/checkbox/TelaDeBoasVindas.java
+++ app/src/main/java/br/fecapccp/checkbox/TelaDeBoasVindas.java
@@ -1,13 +1,6 @@
 package br.fecapccp.checkbox;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class TelaDeBoasVindas extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_tela_de_boas_vindas);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-
-
 }
